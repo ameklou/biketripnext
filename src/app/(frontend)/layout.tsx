f16c49a2ba52693653baf7276530ee554666b6b5
Eklu Amemasovo <amekley@gmail.com>
2025-11-12 14:16:07 +0000

    hero added

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import './styles.css'
+import './global.css'
+import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -10,8 +12,17 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
-      <body>
+    <html lang="fr">
+    <head>
+      <style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily};
+  --font-sans: ${GeistSans.variable};
+  --font-mono: ${GeistMono.variable};
+}
+        `}</style>
+    </head>
+      <body className={"dark"}>
         <main>{children}</main>
       </body>
     </html>
--- vitest.config.mts
+++ vitest.config.mts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react()
+  ],
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
